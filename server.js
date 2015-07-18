aacc82b15c2e58c01488190c033fbccf28b9b3cf
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-18 01:05:46 -0500

    return all posts(model) in a GET request to /api/posts

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var Post = require('./models/post');
 var app = express();
 app.use(bodyParser.json());
 
-app.get('/api/posts', function (req, res) {
-  res.json([{
-    username: 'dickeyxxx',
-    body: 'node rocks!'
-  }]);
+app.get('/api/posts', function (req, res, next) {
+  Post.find(function (err, posts) {
+    if(err) return next(err);
+    res.json(posts);
+  });
 });
 
 app.post('/api/posts', function (req, res, next) {
