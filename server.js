6932c9d126d81d324a8e9050675339adc97ee583
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-18 01:51:31 -0500

    Add posts controller and move its routes in controller

--- server.js
+++ server.js
@@ -4,30 +4,12 @@ var express = require('express'),
 var Post = require('./models/post');
 
 var app = express();
+
 app.use(bodyParser.json());
+app.use(require('./controllers/api/posts'));
 
 app.get('/', function (req, res, next) {
   res.sendfile('layouts/posts.html');
-});
-
-app.get('/api/posts', function (req, res, next) {
-  Post.find()
-  .sort('-date')
-  .exec(function (err, posts) {
-    if (err) return next(err);
-    res.json(posts);
-  });
-});
-
-app.post('/api/posts', function (req, res, next) {
-  var post = new Post({
-    username: req.body.username,
-    body: req.body.body
-  });
-  post.save(function (err, post) {
-    if (err) return next(err);
-    res.json(201,post);
-  });
 });
 
 app.listen(3000, function () {
