8ad71581d6dfc4bfac50ee7df69e2e717e708f5b
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-18 23:04:38 -0500

    Add gulp task 'js' to build front-end javascript

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+
+gulp.task('js',function () {
+  gulp.src('ng/**/*.js')
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('assets'));
+});
