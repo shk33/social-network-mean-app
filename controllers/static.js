48f2ae51106541e2e9e4bc507e9c66d1c00c8703
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-19 16:48:58 -0500

    Add angular routes and move posts.html to its own file

--- controllers/static.js
+++ controllers/static.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.use(express.static(__dirname+'/../assets'));
+router.use(express.static(__dirname+'/../templates'));
 
 router.get('/', function (req, res, next) {
   res.sendfile('layouts/app.html');
