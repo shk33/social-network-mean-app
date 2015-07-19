70d736db0c65e4d17ba15bd655abdef72e2be1c6
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-19 00:30:19 -0500

    Add stylus and gulp task to build it

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 var sourcemaps = require('gulp-sourcemaps');
+var fs = require('fs');
+
+fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
+  require('./gulp/' + task);
+});
 
 gulp.task('js',function () {
   gulp.src(['ng/module.js','ng/**/*.js'])
