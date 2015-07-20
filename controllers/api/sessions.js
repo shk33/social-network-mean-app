8e50a3346aca4e8ca09ddfaffd6749fa6bf81eae
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-19 19:47:32 -0500

    Angular makes a POST request to api/sessions it receives the jwt token and then make a GET request and it recieves the user data

--- controllers/api/sessions.js
+++ controllers/api/sessions.js
@@ -1,7 +1,7 @@
 var router = require('express').Router();
 var User = require('../../models/user');
 var bcrypt = require('bcrypt');
-var jwt = require('jwt');
+var jwt = require('jwt-simple');
 var config = require('../../config');
 
 router.post('/', function (req, res, next) {
@@ -13,8 +13,8 @@ router.post('/', function (req, res, next) {
     bcrypt.compare(req.body.password, user.password, function (err, valid) {
       if (err) return next(err);
       if(!valid) return res.send(401);
-      var token = jwt.encode({username: user.username}, cofig.secret);
-      res.json(token);
+      var token = jwt.encode({username: user.username}, config.secret);
+      res.send(token);
     });
   });
 });
--- ng/user.svc.js
+++ ng/user.svc.js
@@ -3,8 +3,9 @@ angular.module('app')
   var svc = this;
 
   svc.getUser = function () {
-    return $http.get('/api/users', {
-      headers: {'X-Auth': this.token}
+    return $http.get('/api/users')
+    .then(function (response) {
+      return response.data;
     });
   };
 
@@ -12,8 +13,9 @@ angular.module('app')
     return $http.post('/api/sessions', {
       username: username,
       password: password
-    }).then(function (val) {
-      svc.token = val.data;
+    }).then(function (response) {
+      svc.token = response.data;
+      $http.defaults.headers.common['X-Auth'] = response.data;
       return svc.getUser();
     });
   };
