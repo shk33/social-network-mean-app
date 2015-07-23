bbec35a21e9f4ed342b1e683ae43b6d2964c3c0b
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-23 01:45:58 -0500

    Add test for GET /api/posts

--- test/server/controllers/api/posts.spec.js
+++ test/server/controllers/api/posts.spec.js
@@ -1,8 +1,33 @@
-var expect = require('chai').expect;
-var ctrl = require('../../../../controllers/api/posts');
+var api = require('../../support/api');
+var Post = require('../../../../models/post');
+
 
 describe('controller.api.post', function () {
-  it('exists', function () {
-    expect(ctrl).to.exist;
+
+  beforeEach(function (done) {
+    Post.remove({}, done);
+  });
+
+  describe('GET /api/posts', function () {
+    beforeEach(function (done) {
+      var posts = [
+        {body: 'post1', username: 'mcoronel'},
+        {body: 'post2', username: 'mcoronel'},
+        {body: 'post3', username: 'mcoronel'}
+      ];
+      Post.create(posts, done);
+    });
+
+    it('has 3 posts', function (done) {
+      api.get('/api/posts')
+      .expect(200)
+      .expect(function (posts) {
+        if (posts.body.length !==3) {
+          return "post count should be 3";
+        }
+      })
+      .end(done);
+    });
+
   });
 });
