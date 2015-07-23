3fa9b6f069897b638e52a9fe3dd6ef191da89d93
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-22 21:28:20 -0500

    Add protractor to test a simple visit to home page

---
+++ test/e2e/making_a_post.spec.js
@@ -0,0 +1,11 @@
+describe('making a post', function () {
+  it('logs in and creates a new post', function () {
+    // Go to homepage
+    browser.get('http://localhost:3000');
+    // click 'login'
+    // fill out and submit login form
+    // submit a new post on posts page
+
+    // The user should now see their new posts as the first post on the page
+  });
+});
