66d656470c13ddb5efc8aefed747e6095ffc2f5c
Miguel Coronel <m.coronel.seg@gmail.com>
2015-07-23 01:24:29 -0500

    refactor server.js and add index.js in controllers folder

--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-var express = require('express'),
-    bodyParser = require('body-parser'),
-    logger = require('morgan'),
-    webSockets = require('./websockets');
-
-var Post = require('./models/post');
-
+var express = require('express');
+var logger = require('morgan');
+var websockets = require('./websockets');
 var app = express();
 
-app.use(bodyParser.json());
 app.use(logger('dev'));
-
-app.use(require('./auth'));
-app.use(require('./controllers/static'));
-app.use('/api/posts',require('./controllers/api/posts'));
-app.use('/api/sessions', require('./controllers/api/sessions'));
-app.use('/api/users', require('./controllers/api/users'));
+app.use(require('./controllers'));
 
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
-  console.log('Server', process.pid ,'running at on port', port);
+  console.log('Server', process.pid, 'listening on', port);
 });
-webSockets.connect(server);
+websockets.connect(server);
